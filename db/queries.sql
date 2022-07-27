614bba1af41d103ca235254c0eb2af66ce79fe20
kitanablade <kiteagle@gmail.com>
2022-07-27 16:54:24 -0700

    seeding the db for testing and setup queries file

---
+++ db/queries.sql
@@ -0,0 +1,9 @@
+USE employee_db;
+
+SOURCE ./db/schema.sql;
+SOURCE ./db/seeds.sql;
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE employee_db;
+
+INSERT INTO department (name)
+VALUES ("Back-end"),
+       ("Client-side"),
+       ("Data");
+       
+INSERT INTO role (title, salary, department_id)
+VALUES ("Senior Engineer", 148000.00, 2),
+        ("DBA", 82000.00, 3),
+        ("Staff Engineer", 170000.00, 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Kit", "Williams", 3),
+("Newt", "Ault", 2),
+("Honu", "Bendixen", 1);
