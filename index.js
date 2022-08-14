e9fb14fe0c7267b2d02d5e29dec31354d1a6e42d
kitanablade <kiteagle@gmail.com>
2022-08-14 15:08:21 -0700

    added role update functionality

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ async function selectOptions() {
                         if (err) {
                           console.log(err);
                         }
-                        console.log(`${newEmployeeInfo} added to database.`);
+                        console.log(`${newEmployeeInfo.first_name} ${newEmployeeInfo.last_name} added to database.`);
                         console.log(
                           "================================================================"
                         );
@@ -272,44 +272,72 @@ async function selectOptions() {
       break;
 
     case "Update an Employee Role":
+      let empId = "";
+      let roleId = "";
+      db.query(
+        `SELECT CONCAT(employee.first_name," ", employee.last_name) AS name, employee.id AS value
+                FROM employee`,
+        function (err, empNames) {
+          if (err) {
+            console.log(err);
+          }
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "employee_id",
+                message: "Please select the employee whose role you'd like to update:",
+                choices: empNames,
+              },
+            ]).then((data)=>{
+              console.log("DATA LOG: ",data);
+              empId = data.employee_id;
+              db.query(
+                `SELECT role.title AS name, role.id AS value
+                    FROM role;
+                    `,
+                function (err, newRole) {
+                  if (err) {
+                    console.log(err);
+                  }
+                  inquirer
+                    .prompt([
+                      {
+                        type: "list",
+                        name: "new_role_id",
+                        message: "Please select the employee's new role:",
+                        choices: newRole,
+                      },
+                    ])
+                    .then((data) => {
+                      roleId = data.new_role_id;
+                      db.query(
+                        `UPDATE employee
+                        SET role_id = ${roleId}
+                        WHERE employee.id = ${empId}`,
+                        function (err) {
+                          if (err) {
+                            console.log(err);
+                          }
+                          console.log("Employee's role successfully updated.")
+                          console.log(
+                            "================================================================"
+                          );
+                          selectOptions();
+                        }
+                      );
+                    });
+                }
+              );
+            })
+        }
+      );
       break;
-
+      
     case "Quit":
       console.log("Have a nice day!");
       break;
   }
 }
-
-//can also delete callback function and just do return db.query
-function getManagers() {
-  db.query(
-    `SELECT CONCAT(employee.first_name," ", employee.last_name) AS mgr_fullname, employee.id
-  FROM employee
-  JOIN role ON employee.role_id=role.id
-  WHERE title = "Manager"`,
-    function (err, mgrNames) {
-      if (err) {
-        console.log(err);
-      }
-      return mgrNames;
-    }
-  );
-}
-
-// let newDept = data.dept_name;
-// db.query(
-//   `INSERT INTO department (dept_name) VALUES (?)`,
-//   newDept,
-//   (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(`${newDept} added to database.`);
-//     console.log(
-//       "================================================================"
-//     );
-//     selectOptions();
-//   }
-// );
 
 selectOptions();
